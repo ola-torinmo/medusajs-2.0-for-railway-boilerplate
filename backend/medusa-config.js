f77a1e48366770f269f75bf4966a5a0ece11ed37
torinmo <torinmo.work@gmailcom>
2025-09-26 15:43:35 +0100

    aws config

--- backend/medusa-config.js
+++ backend/medusa-config.js
@@ -99,6 +99,7 @@ const medusaConfig = {
     secret_access_key: process.env.AWS_SECRET_ACCESS_KEY,
     region: process.env.AWS_REGION,
     bucket: process.env.AWS_S3_BUCKET,
+   s3_url: `https://satori-medusa.s3.eu-north-1.amazonaws.com`,
   },
 }
 
